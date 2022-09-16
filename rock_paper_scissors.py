62916196a03728599b910543702d13eb9067cd5b
Brian Hill <brianhill401@gmail.com>
2022-09-16 05:44:13 -0400

    rock_paper_scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,10 +2,16 @@
 # two player rock paper scissors game
 
 while True:
-    print("Let's play Rock-Paper-Scissors!")
+    print("Let's play Rock-Paper-Scissors!\nPress 'exit' to quit.")
     print()
-    player_one = input("Player One, you go first. ").lower()
+    player_one = input("Player One, you go. ").lower()
+    if player_one.lower() == "exit":
+        print("Goodbye.")
+        break
     player_two = input("It's your turn Player Two. ").lower()
+    if player_two.lower() == "exit":
+        print("Goodbye.")
+        break
     if player_one == "rock" and player_two == "scissors":
         print("Player One wins!")
     elif player_one == "scissors" and player_two == "paper":
@@ -15,5 +21,9 @@ while True:
     else:
         print("Player Two wins!")
     print()
+
+
+
+
 
 
