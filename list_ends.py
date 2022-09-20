73d3d68c4bbc9eb1e3eb585b646718c90f83410c
Brian Hill <brianhill401@gmail.com>
2022-09-20 05:37:59 -0400

    list_ends

---
+++ list_ends.py
@@ -0,0 +1,12 @@
+
+# a function that takes a list of numbers and makes a new list of the
+# first and last elements of the given list.
+
+def endlist(a_list):
+    return [a[0], a[-1]]
+
+a = [5, 10, 15, 20, 25, 30]
+print(endlist(a))
+
+
+
